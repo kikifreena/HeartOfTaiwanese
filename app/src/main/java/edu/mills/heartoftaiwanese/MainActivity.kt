69bdc245b43bf318218a43a2b408fe6d948ee105
kikifreena <14kobarbara@gmail.com>
2020-09-07 14:18:59 -0700

    Add error case for rate limited English-Chinese translation

--- app/src/main/java/edu/mills/heartoftaiwanese/MainActivity.kt
+++ app/src/main/java/edu/mills/heartoftaiwanese/MainActivity.kt
@@ -8,8 +8,6 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.BufferedReader
-import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
 
@@ -22,15 +20,13 @@ class MainActivity : AppCompatActivity() {
         submit_ch.setOnClickListener {
             tw_result.visibility = View.GONE
             result.visibility = View.GONE
-            ParseWordTask().execute(LanguageContainer(editText
-                    .text.toString(), LANGUAGE_CHINESE)
-            )
+            ParseWordTask().execute(LanguageContainer(editText.text.toString(), LANGUAGE_CHINESE))
         }
         submit_en.setOnClickListener {
             findViewById<View>(R.id.tw_result).visibility = View.GONE
             findViewById<View>(R.id.result).visibility = View.GONE
-            ParseWordTask().execute(LanguageContainer(editTextEng
-                    .text.toString(), LANGUAGE_ENGLISH)
+            ParseWordTask().execute(
+                LanguageContainer(editTextEng.text.toString(), LANGUAGE_ENGLISH)
             )
         }
         val clearButton = findViewById<Button>(R.id.clear)
@@ -42,9 +38,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private data class LanguageContainer internal constructor(internal val text: String, internal val language: Int)
+    private data class LanguageContainer internal constructor(
+        internal val text: String,
+        internal val language: Int
+    )
 
-    private inner class ParseWordTask : AsyncTask<LanguageContainer, Int, Boolean>() {
+    private inner class ParseWordTask : AsyncTask<LanguageContainer, Int, String>() {
         /**
          * Override this method to perform a computation on a background thread. The
          * specified parameters are the parameters passed to [.execute]
@@ -61,26 +60,42 @@ class MainActivity : AppCompatActivity() {
          */
         private lateinit var w: Word
 
-        override fun doInBackground(vararg lang: LanguageContainer): Boolean? {
-            if (lang[0].language == LANGUAGE_ENGLISH){
-                val result = fetchChinese(lang[0].text)
-                w = Word(result)
-            }
-            else {
+        override fun doInBackground(vararg lang: LanguageContainer): String? {
+            if (lang[0].language == LANGUAGE_ENGLISH) {
+                val chineseString = fetchChinese(lang[0].text)
+                if (chineseString == kUnknownError) {
+                    return kUnknownError
+                } else if (chineseString == kRateLimited) {
+                    return kRateLimited
+                } else {
+                    w = Word(chineseString)
+                }
+//                Log.d("MainActivity", w.toString())
+            } else {
                 w = Word(lang[0].text)
             }
-            return w.run()
+            val status = w.run()
+            if (!status) {
+                return kUnknownError
+            } else {
+                return kResultOk
+            }
         }
 
-        override fun onPostExecute(status: Boolean) {
-            if (status) {
+        override fun onPostExecute(status: String) {
+            if (status == kRateLimited) {
+                Toast.makeText(
+                    this@MainActivity,
+                    getText(R.string.too_many_requests),
+                    Toast.LENGTH_LONG
+                ).show()
+            } else if (status == kUnknownError) {
+                Toast.makeText(this@MainActivity, getText(R.string.error), Toast.LENGTH_LONG).show()
+            } else {
                 Log.d("MainActivity-Taiwanese", w.toString())
                 tw_result.visibility = View.VISIBLE
                 result.visibility = View.VISIBLE
                 result.text = w.taiwanese
-            } else {
-                Toast.makeText(this@MainActivity,
-                    this@MainActivity.getText(R.string.error).toString(), Toast.LENGTH_LONG).show()
             }
             super.onPostExecute(status)
         }
@@ -89,20 +104,32 @@ class MainActivity : AppCompatActivity() {
     companion object {
         const val LANGUAGE_ENGLISH = 1
         const val LANGUAGE_CHINESE = 0
-        private const val URL_TO_CRAWL_ENCH = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=";
+        private const val URL_TO_CRAWL_ENCH =
+            "https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=en";
+        private const val kUnknownError = "UNKNOWN ERROR"
+        private const val kRateLimited = "RATE LIMITED"
+        private const val kResultOk = "SUCCESS"
     }
 
-    protected fun fetchChinese(english: String): String? {
-        val url = URL("${URL_TO_CRAWL_ENCH}zh&tl=en&dt=t&q=$english")
+    protected fun fetchChinese(english: String): String {
+        Log.d("MainActivity", "Fetching Chinese for $english")
+        val url = URL("${URL_TO_CRAWL_ENCH}&dt=t&q=$english")
+        // https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=zh&dt=t&q=hello
         val connection = url.openConnection() as HttpURLConnection
-        connection.requestMethod = "POST"
+        connection.requestMethod = "GET"
         connection.setRequestProperty("Accept-Charset", "UTF-8")
+        Log.d("MainActivity", connection.responseCode.toString())
         if (connection.responseCode == HttpURLConnection.HTTP_OK) {
             val inputAsString = connection.inputStream.bufferedReader().use { it.readText() }
             val start = inputAsString.indexOf('"')
-            val stop = inputAsString.indexOf('"', start)
+            val stop = inputAsString.indexOf('"', start + 1)
             return inputAsString.substring(start + 1, stop)
+        } else if (connection.responseCode == 429) {
+            Log.d("MainActivity", "too many HTTP requests")
+            return kRateLimited
+        } else {
+            Log.i("MainActivity", connection.responseMessage)
+            return kUnknownError
         }
-        return null
     }
 }
