3a6e223656b7963ab02c996da8b1d392d0604a8b
kikifreena <14kobarbara@gmail.com>
2021-03-06 00:27:06 -0800

    Add recyclerview adapter

---
+++ app/src/main/java/edu/mills/heartoftaiwanese/activity/adapter/WordListAdapter.kt
@@ -0,0 +1,124 @@
+package edu.mills.heartoftaiwanese.activity.adapter
+
+import android.content.res.ColorStateList
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import edu.mills.heartoftaiwanese.R
+import edu.mills.heartoftaiwanese.data.DatabaseWord
+import edu.mills.heartoftaiwanese.data.Word
+import edu.mills.heartoftaiwanese.databinding.LayoutWordItemBinding
+import java.text.DateFormat
+
+class WordListAdapter :
+    ListAdapter<DatabaseWord, WordListAdapter.WordListViewHolder>(WordsCallback) {
+
+    interface FavoriteButtonListener {
+        fun onFavoriteClicked()
+    }
+
+    class WordListViewHolder(private val binding: LayoutWordItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        private val dateFormat = DateFormat.getDateInstance(DateFormat.LONG)
+
+        private var isFavorite: Boolean = false
+            set(value) {
+                field = value
+                if (value) {
+                    // Display the favorite button as colorPrimary
+                    binding.favoriteSelector.imageTintList = ColorStateList.valueOf(
+                        binding.root.context.resources.getColor(R.color.colorPrimary, null)
+                    )
+                } else {
+                    // Unfavorite; clear the favorite button.
+                    binding.favoriteSelector.imageTintList = null
+                }
+            }
+
+        fun bind(databaseWord: DatabaseWord) {
+            displayWord(databaseWord.word)
+            isFavorite = databaseWord.favorite
+            binding.tvLastAccessed.text = dateFormat.format(databaseWord.accessTime)
+            binding.favoriteSelector.setOnClickListener {
+
+            }
+        }
+
+        private fun displayWord(word: Word) {
+            binding.tvRightText.text = word.taiwanese
+            binding.tvWordTitle.text = word.taiwanese
+
+            binding.tvCenterText.text = word.chinese
+            binding.tvLeftText.text = word.english
+        }
+    }
+
+    /**
+     * Called when RecyclerView needs a new [ViewHolder] of the given type to represent
+     * an item.
+     *
+     *
+     * This new ViewHolder should be constructed with a new View that can represent the items
+     * of the given type. You can either create a new View manually or inflate it from an XML
+     * layout file.
+     *
+     *
+     * The new ViewHolder will be used to display items of the adapter using
+     * [.onBindViewHolder]. Since it will be re-used to display
+     * different items in the data set, it is a good idea to cache references to sub views of
+     * the View to avoid unnecessary [View.findViewById] calls.
+     *
+     * @param parent The ViewGroup into which the new View will be added after it is bound to
+     * an adapter position.
+     * @param viewType The view type of the new View.
+     *
+     * @return A new ViewHolder that holds a View of the given view type.
+     * @see .getItemViewType
+     * @see .onBindViewHolder
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordListViewHolder {
+        return WordListViewHolder(
+            LayoutWordItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    /**
+     * Called by RecyclerView to display the data at the specified position. This method should
+     * update the contents of the [ViewHolder.itemView] to reflect the item at the given
+     * position.
+     *
+     *
+     * Note that unlike [android.widget.ListView], RecyclerView will not call this method
+     * again if the position of the item changes in the data set unless the item itself is
+     * invalidated or the new position cannot be determined. For this reason, you should only
+     * use the `position` parameter while acquiring the related data item inside
+     * this method and should not keep a copy of it. If you need the position of an item later
+     * on (e.g. in a click listener), use [ViewHolder.getAdapterPosition] which will
+     * have the updated adapter position.
+     *
+     * Override [.onBindViewHolder] instead if Adapter can
+     * handle efficient partial bind.
+     *
+     * @param holder The ViewHolder which should be updated to represent the contents of the
+     * item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
+    override fun onBindViewHolder(holder: WordListViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
+
+object WordsCallback : DiffUtil.ItemCallback<DatabaseWord>() {
+    override fun areItemsTheSame(oldWord: DatabaseWord, newWord: DatabaseWord): Boolean {
+        // User properties may have changed if reloaded from the DB, but ID is fixed
+        return oldWord.id == newWord.id
+    }
+
+    override fun areContentsTheSame(oldWord: DatabaseWord, newWord: DatabaseWord): Boolean {
+        return oldWord == newWord
+    }
+}
--- app/src/main/java/edu/mills/heartoftaiwanese/data/DatabaseWord.kt
+++ app/src/main/java/edu/mills/heartoftaiwanese/data/DatabaseWord.kt
@@ -2,6 +2,12 @@ package edu.mills.heartoftaiwanese.data
 
 import java.util.Date
 
+/**
+ * @property word
+ * @property id
+ * @property favorite
+ * @property accessTime
+ */
 data class DatabaseWord(
     val word: Word,
     val id: Int,
